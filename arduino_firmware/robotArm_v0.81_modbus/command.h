68ff06c7db6a03545ee7a66e2dc51d10c17d2baf
michaelstothard <102836695+michaelstothard@users.noreply.github.com>
2022-04-04 20:20:23 +1000

    Added modbus support

---
+++ arduino_firmware/robotArm_v0.81_modbus/command.h
@@ -0,0 +1,43 @@
+#ifndef COMMAND_H_
+#define COMMAND_H_
+
+#include <Arduino.h>
+#include "interpolation.h"
+
+struct Cmd {
+  char id;
+  int num;
+  float valueX;
+  float valueY;
+  float valueZ;
+  float valueF;
+  float valueE;
+  float valueS;
+};
+
+class Command {
+  public:
+    Command();
+    bool handleGcode();
+    bool processMessage(String msg);
+    void value_segment(String msg_segment);
+    Cmd getCmd() const;
+    void cmdGetPosition(Point pos, Point pos_offset, float highRad, float lowRad, float rotRad);
+    void cmdToRelative();
+    void cmdToAbsolute();
+    bool isRelativeCoord;
+    Cmd new_command;
+    String modbus_gcode;
+    int lastmodbus_array = 0;
+    int currentmodbus_array = 0;
+
+
+  private:
+    String message;
+};
+
+void cmdMove(Cmd(&cmd), Point pos, Point pos_offset, bool isRelativeCoord);
+void cmdDwell(Cmd(&cmd));
+void printErr();
+
+#endif
